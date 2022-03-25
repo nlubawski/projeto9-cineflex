8ff2e3676ebf08d406c39f54dd73c96fb0ed7cef
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-03-25 15:32:58 -0300

    Em Sessao os assentos sao renderizados na tela e em algumas partes do componente foi aplicado o layout conforme figma

--- src/componentes/Sessao.jsx
+++ src/componentes/Sessao.jsx
@@ -1,12 +1,10 @@
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 
-function Sessao(){
-    const params = useParams()
-    const [sessao, setSessao] = useState([]);
-    console.log(params.idSessao)
+function Sessao() {
+  const params = useParams();
+  const [sessao, setSessao] = useState([]);
 
   useEffect(() => {
     const requisicao = axios.get(
@@ -14,38 +12,61 @@ function Sessao(){
     );
 
     requisicao.then((resposta) => {
-      console.log(resposta.data)
       setSessao(resposta.data);
       requisicao.catch((erro) => console.log(erro));
     });
   }, []);
 
-    return <></>
-//   return (
-//       <>
-//     <main className="sessao">
-//       <section className="sessao__titulo">
-//         <p>Selecione o(s) assento(s)</p>
-//       </section>
-//       <section className="sessao__lista">
-//         <ul className="sessao__lista__itens">
-//           {filmes.map((filme) => (
-//             <Link to={`/filme/${filme.id}`} key={filme.id}>
-//               <li >
-//                 <img src={filme.posterURL} alt={filme.title} />
-//               </li>
-//             </Link>
-//           ))}
-//         </ul>
-//       </section>
-//     </main>
-//             <footer>
-//             <img src={posterURL} alt={title} />
-//             <p>{title}</p>
-//           </footer>
-//           </>
-//   );
+  return (
+    <>
+      <main className="sessao">
+        <section className="sessao__titulo">
+          <p>Selecione o(s) assento(s)</p>
+        </section>
+        <section className="sessao__assentos">
+          {sessao.movie !== undefined ? (
+            <>
+              {sessao.seats.map((num) => {
+                return (
+                  <div className="sessao__assentos__unidade">
+                    <p>{num.name}</p>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            <></>
+          )}
+        </section>
+        <section className="sessao__legenda">legendas aqui</section>
+        <section>
+          <div className="sessao__comprador">
+            <p>Nome do comprador:</p>
+            <input type="text" />
+          </div>
+          <div className="sessao__cpf">
+            <p>CPF do comprador:</p>
+            <input type="text" />
+          </div>
+          <div className="sessao__botao">
+              <button>Reservar Acentos</button>
+          </div>
+          
+        </section>
+      </main>
+      <footer>
+        {sessao.movie !== undefined ? (
+          <>
+            {console.log(sessao.seats)}
+            <img src={sessao.movie.posterURL} alt={sessao.movie.title} />
+            <p>{sessao.movie.title}</p>
+          </>
+        ) : (
+          <></>
+        )}
+      </footer>
+    </>
+  );
 }
 
-export default Sessao
-
+export default Sessao;
